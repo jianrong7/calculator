7029081feaf6eb3e9439c9cd7f285749ed6298a8
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-05-11 11:30:09 +0800

    Added night mode feature. Border not working yet.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var checkbox = document.querySelector("input[name=checkbox]");
+
 function add(a, b) {
     return a + b;
 }
@@ -33,4 +35,56 @@ function buttonPressed(btn) {
     // setTimeout(removeClicked.bind(null, btn), 100);
     setTimeout(() => {btn.classList.remove('clicked')}, 100);
 }
-  
+/* NIGHT MODE*/
+checkbox.addEventListener("change", function() {
+    const border = document.getElementById("calculator");
+    const header = document.querySelector("#calculatorHeader");
+    const output = document.querySelector("#output");
+    const number = document.querySelector("#number");
+    const calcInput = document.querySelector("#calculatorInput");
+    const operatorInputs = document.querySelectorAll(".input");
+    const numberInputs = document.querySelectorAll(".numInput");
+    const equal = document.querySelector("#equalBtn");
+
+    if (this.checked) {
+        border.style.borderColor = "#212b41;"
+        header.style.backgroundColor = "#212b41";
+        output.style.backgroundColor = "#212b41";
+        number.style.color = "#18d4a3";
+        calcInput.style.backgroundColor = "#2d384f";
+
+        operatorInputs.forEach((operatorInput) => {
+            operatorInput.style.backgroundColor = "#212b41";
+            operatorInput.style.color = "#18d4a3";
+        })
+
+        numberInputs.forEach((numberInput) => {
+            numberInput.style.backgroundColor = "#2d384f";
+            numberInput.style.color = "#96a8a0";
+        })
+
+        equal.style.backgroundColor = "#18d4a3";
+        equal.style.color = "#333333";
+    }
+    else {
+        border.style.border = "grey;"
+        header.style.backgroundColor = "#f4fdfb";
+        output.style.backgroundColor = "#f4fdfb";
+        number.style.color = "#373e47";
+        calcInput.style.backgroundColor = "white";
+
+        operatorInputs.forEach((operatorInput) => {
+            operatorInput.style.backgroundColor = "#f4fdfb";
+            operatorInput.style.color = "#333333";
+        })
+
+        numberInputs.forEach((numberInput) => {
+            numberInput.style.backgroundColor = "#f4fdfb";
+            numberInput.style.color = "#333333";
+        })
+
+        equal.style.backgroundColor = "#18d4a3";
+        equal.style.color = "white";
+    }
+})
+/* NIGHT MODE */
