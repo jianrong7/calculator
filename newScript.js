391daabccc330cb696c013510ad3be7d18be33a8
jianrong7 <ipwnjianrong@gmail.com>
2021-03-28 21:50:54 +0800

    Added functionatlity for first operation

--- newScript.js
+++ newScript.js
@@ -1,28 +1,97 @@
-function add(a, b) {
-    return a + b;
-}
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
-function divide(a, b) {
-    return a / b;
-}
+let waitingForSecondOperand = false
+let waitingForEqual = false
+let firstOperand = ""
+let secondOperand = ""
+let answer = ""
+let operator = ""
+
+// function add(a, b) {
+//     return a + b;
+// }
+// function subtract(a, b) {
+//     return a - b;
+// }
+// function multiply(a, b) {
+//     return a * b;
+// }
+// function divide(a, b) {
+//     return a / b;
+// }
 function operate(a, b, operator) {
+    a = parseInt(a)
+    b = parseInt(b)
+    console.log(a)
+    console.log(b)
+    console.log(operator)
     switch(operator) {
         case "+":
-            add(a, b)
-            break
+            return a + b
         case "-":
-            subtract(a, b)
-            break
+            return a - b
         case "*":
-            multiply(a, b)
-            break
+            return a * b
         case "/":
-            divide(a, b)
-            break
+            return a / b
     }
 }
+// function updateDisplay(displayValue) {
+//     const display = document.querySelector('.calculator-screen')
+//     displayValue = parseInt(displayValue)
+
+//     if (Number.isNaN(displayValue)) {
+//         return
+//     } else {
+//         if (display.Value == "") {
+//             display.value = displayValue
+//         } else {
+//             display.value += displayValue
+//         }
+//     }
+// }
+// function handleOperator(key, firstNumber) {
+//     // if (key.value == )
+//     console.log(firstNumber)
+// }
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen')
+    if (!waitingForSecondOperand) {
+        display.value = firstOperand
+    } else if (waitingForEqual) {
+        display.value = answer
+    } else {
+        display.value = secondOperand
+    }
+}
+function updateOperand(key) {
+    if (!waitingForSecondOperand) {
+        firstOperand += key.value
+    } else {
+        secondOperand += key.value
+    }
+}
+function inputDigit() {
+    const keys = document.querySelectorAll('.input')
+    const display = document.querySelector('.calculator-screen')
+    keys.forEach(key => {
+        key.addEventListener('click', () => {
+            if (key.classList.contains('number')) {
+                updateOperand(key)
+                updateDisplay()
+            }
+            if (key.classList.contains('operators')) {
+                waitingForSecondOperand = true
+                operator = key.value
+                console.log(operator)
+                updateDisplay()
+            }
+            if (key.classList.contains('equal')) {
+                waitingForEqual = true                
+                answer = operate(firstOperand, secondOperand, operator)
+                console.log(answer)
+                updateDisplay()
+                waitingForSecondOperand = false
+            }
+        })
+    })
+}
+inputDigit()
