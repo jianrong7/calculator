410524b32e489471a77973d81a3b1021d0bd476f
jianrong7 <ipwnjianrong@gmail.com>
2021-03-29 21:45:02 +0800

    Revamped basic functionality with cleaner code

--- newScript.js
+++ newScript.js
@@ -1,13 +1,80 @@
-let waitingForSecondOperand = false
-let waitingForEqual = false
-let firstOperand = ""
-let secondOperand = ""
-let answer = ""
-let operator = ""
+// function handleEqual() { [MATHEMATICALLY CORRECT VERSION - GOOGLE CALC VERSION]
+//     while (storedOperandArray.length > 1) {
+//         let answer = ""
+//         let divisorIndex = storedOperandArray.indexOf('/')
+//         let multiplyIndex = storedOperandArray.indexOf('*')
 
+//         if (divisorIndex < 0 && multiplyIndex < 0) {
+//             let plusIndex = storedOperandArray.indexOf('+')
+//             let minusIndex = storedOperandArray.indexOf('-')
+
+//             if (plusIndex > 0 && minusIndex > 0) {
+//                 if (plusIndex > minusIndex) {
+//                     answer = operate(storedOperandArray[minusIndex-1],
+//                         storedOperandArray[minusIndex+1],
+//                         storedOperandArray[minusIndex])
+//                     storedOperandArray.splice(minusIndex-1, 3, answer)
+//                     console.log(storedOperandArray)
+//                     continue
+//                 } else {
+//                     answer = operate(storedOperandArray[plusIndex-1],
+//                         storedOperandArray[plusIndex+1],
+//                         storedOperandArray[plusIndex])
+//                     storedOperandArray.splice(plusIndex-1, 3, answer)
+//                     console.log(storedOperandArray)
+//                     continue
+//                 }
+//             } else if (plusIndex > 0 && minusIndex < 0) {
+//                 answer = operate(storedOperandArray[plusIndex-1],
+//                     storedOperandArray[plusIndex+1],
+//                     storedOperandArray[plusIndex])
+//                 storedOperandArray.splice(plusIndex-1, 3, answer)
+//                 console.log(storedOperandArray)
+//                 continue
+//             } else if (plusIndex < 0 && minusIndex > 0) {
+//                 answer = operate(storedOperandArray[minusIndex-1],
+//                     storedOperandArray[minusIndex+1],
+//                     storedOperandArray[minusIndex])
+//                 storedOperandArray.splice(minusIndex-1, 3, answer)
+//                 console.log(storedOperandArray)
+//                 continue
+//             }                        
+//         } else if (divisorIndex > 0 && multiplyIndex > 0) {
+//             if (divisorIndex > multiplyIndex) {
+//                 answer = operate(storedOperandArray[multiplyIndex-1],
+//                     storedOperandArray[multiplyIndex+1],
+//                     storedOperandArray[multiplyIndex])
+//                 storedOperandArray.splice(multiplyIndex-1, 3, answer)
+//                 console.log(storedOperandArray)
+//                 continue
+//             } else {
+//                 answer = operate(storedOperandArray[divisorIndex-1],
+//                     storedOperandArray[divisorIndex+1],
+//                     storedOperandArray[divisorIndex])
+//                 storedOperandArray.splice(divisorIndex-1, 3, answer)
+//                 console.log(storedOperandArray)
+//                 continue
+//             }
+//         } else if (divisorIndex > 0 && multiplyIndex < 0) {
+//             answer = operate(storedOperandArray[divisorIndex-1],
+//                 storedOperandArray[divisorIndex+1],
+//                 storedOperandArray[divisorIndex])
+//             storedOperandArray.splice(divisorIndex-1, 3, answer)
+//             console.log(storedOperandArray)
+//             continue
+//         } else if (divisorIndex < 0 && multiplyIndex > 0) {
+//             answer = operate(storedOperandArray[multiplyIndex-1],
+//                 storedOperandArray[multiplyIndex+1],
+//                 storedOperandArray[multiplyIndex])
+//             storedOperandArray.splice(multiplyIndex-1, 3, answer)
+//             console.log(storedOperandArray)
+//             continue
+//         }
+//     }
+// }
 function operate(a, b, operator) {
-    a = parseInt(a)
-    b = parseInt(b)
+    a = parseFloat(a)
+    b = parseFloat(b)
     let temp = 0
     switch(operator) {
         case "+":
@@ -24,58 +91,69 @@ function operate(a, b, operator) {
             return temp.toString()
     }
 }
+let displayValue = "";
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+let waitingForSecondOperand = false
+const numberRegex = /\d+/g;
+
+function handleNumber(keyValue) {
+    if (waitingForSecondOperand) {
+        secondOperand += keyValue
+    } else {
+        firstOperand += keyValue
+    }
+    displayValue += keyValue
+}
+function handleOperator(keyValue) {
+    if (waitingForSecondOperand) {
+        let result = operate(firstOperand, secondOperand, operator)
+        displayValue = result
+        firstOperand = result
+        secondOperand = ""
+    }  
+    operator = keyValue
+    waitingForSecondOperand = true
+}
+function handleEqual() {
+    let result = operate(firstOperand, secondOperand, operator)
+    displayValue = result
+    firstOperand = result
+    secondOperand = ""
+    waitingForSecondOperand = false
+}
+function reset() {
+    displayValue = "0";
+    firstOperand = "";
+    secondOperand = "";
+    operator = "";
+    waitingForSecondOperand = false
+}
 function updateDisplay() {
     const display = document.querySelector('.calculator-screen')
-    if (!waitingForSecondOperand) {
-        display.value = firstOperand
-    } else if (waitingForEqual) {
-        display.value = answer
-    } else {
-        display.value = secondOperand
-    }
+    display.value = displayValue
 }
-function updateOperand(key) {
-    if (!waitingForSecondOperand) {
-        firstOperand += key.value
-    } else {
-        secondOperand += key.value
-    }
-}
-function inputDigit() {
-    const keys = document.querySelectorAll('.input')
-    keys.forEach(key => {
-        key.addEventListener('click', () => {
+
+const keys = document.querySelectorAll('.input')
+keys.forEach(key => {
+    key.addEventListener('click', () => {
             if (key.classList.contains('number')) {
-                updateOperand(key)
+                handleNumber(key.value)
                 updateDisplay()
             }
             if (key.classList.contains('operators')) {
-                waitingForSecondOperand = true
-                operator = key.value
-                console.log(operator)
+                handleOperator(key.value)
                 updateDisplay()
+                displayValue = ""
             }
             if (key.classList.contains('equal')) {
-                waitingForEqual = true                
-                answer = operate(firstOperand, secondOperand, operator)
-                console.log(answer)
+                handleEqual()
                 updateDisplay()
-                
-                waitingForSecondOperand = false
-                waitingForEqual = false
-                firstOperand = answer
-                secondOperand = ""
             }
             if (key.classList.contains('clear')) {
-                waitingForSecondOperand = false
-                waitingForEqual = false
-                firstOperand = ""
-                secondOperand = ""
-                answer = ""
-                operator = ""
+                reset()
                 updateDisplay()
             }
-        })
     })
-}
-inputDigit()
+})
